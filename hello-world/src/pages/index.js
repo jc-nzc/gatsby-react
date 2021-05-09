0c4040822b5191e1f1743c83d0f55012066d4048
Zero <node.zerocool@gmail.com>
2021-05-08 22:29:35 -0500

    unsplash img

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
     <div style={{ color: `gray`, fontSize: `48px`, textAlign: `center` }}>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="Use Your Imagination" />
     </div>
   );
 }
