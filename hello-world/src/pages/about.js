ad851989832f0974b7b3e623a594b0c5c1b65f05
Zero <node.zerocool@gmail.com>
2021-05-10 16:19:02 -0500

    multi props

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 export default function About() {
 	return(
 		<div style={{ color: `red` }}>
-			<Header headerText="About Gatsby" />
+			<Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
 			<p>Such wow, very Reactive.</p>
 		</div>
 	)
