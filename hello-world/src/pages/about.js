6b9d284dbe6d034390a3d20524be6d321c7658e7
Zero <node.zerocool@gmail.com>
2021-05-09 20:49:50 -0500

    about

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+	return(
+		<div style={{ color: `red` }}>
+			<h1> About Jorge & Co </h1>
+			<p>Such wow, very Reactive.</p>
+		</div>
+	)
+}
