d895bb6d6d40bb0f83a1a479f8da0013fe3abd57
Zero <node.zerocool@gmail.com>
2021-05-11 17:58:24 -0500

    + header text

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -5,6 +5,7 @@ export default function About() {
 	return(
 		<div style={{ color: `red` }}>
 			<Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
+			<Header headerText="It's pretty cool" />
 			<p>Such wow, very Reactive.</p>
 		</div>
 	)
