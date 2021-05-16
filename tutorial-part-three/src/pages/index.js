fdb0e2eb632041f46c7754cef429e405a6225aa2
Zero <node.zerocool@gmail.com>
2021-05-15 23:51:10 -0500

    nested layout components

--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ fontSize: `24px`, textAlign: `center`, paddingTop: `48px` }}>
+      <p>Hello World!</p>
+      <br/>
+      <p>Programmer Happiness is Key</p>
+    </div>
+  );
 }
